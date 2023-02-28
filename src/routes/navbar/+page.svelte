86a6bcb515640a4d9e498af6000db48aa1290421
Sabbirz <sabbir.kuasha@gmail.com>
2023-02-28 19:48:59 +0600

    log in log out

---
+++ src/routes/navbar/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { toggle_cart } from './toggle';
+
+	// $: console.log($toggle_cart);
+
+	function toggle1() {
+		// closeMiniCart();
+
+		toggle_cart.set(!$toggle_cart);
+	}
+</script>
+
+<button on:click={toggle1} class="btn btn-primary"> On click</button>
+
+<h1>Status {$toggle_cart}</h1>
---
+++ src/routes/navbar/toggle.js
@@ -0,0 +1,3 @@
+import { writable } from 'svelte/store';
+
+export const toggle_cart = writable(false);
